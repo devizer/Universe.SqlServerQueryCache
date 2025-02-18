edf2dddc1cea0bad3d7578bc28d333d4d91f7323
devizer <devizer@github.io>
2025-02-18 11:23:20 +0200

    Sorting: Brush up button click handler

--- Universe.SqlServerQueryCache/Exporter/Main.js
+++ Universe.SqlServerQueryCache/Exporter/Main.js
@@ -3,33 +3,35 @@
     var sortButtons = document.querySelectorAll("button.SortButton");
     sortButtons.forEach(function (b) {
         b.addEventListener("click", function(sortButtonClickEvent) {
-            console.log("Sort Clicked", sortButtonClickEvent);
+            // console.log("Sort Clicked", sortButtonClickEvent);
         });
-        console.log("[Sort Button] Click Listener added for: " + b.innerText, b);
+        // console.log("[Sort Button] Click Listener added for: " + b.innerText, b);
     });
     var tables = document.querySelectorAll("table.Metrics");
-    console.log("Tables", tables);
+    // console.log("Tables", tables);
     tables.forEach(function (t) {
         t.addEventListener("click", function (tableClickEvent) {
             console.log("[CLICK] TABLE CLICKED '" + tableClickEvent.target.tagName + "'", tableClickEvent);
             var sortParameter = tableClickEvent.target.dataset.sorting;
             console.log("[CLICK] TARGET DATASET", tableClickEvent.target.dataset);
             var parent = tableClickEvent.target.parentElement;
-            console.log("[CLICK] TARGET's PARENT", parent);
-            console.log("[CLICK] TARGET's PARENT DATASET", parent.dataset);
-            sortParameter = parent.dataset.sorting;
+            if (parent) {
+                console.log("[CLICK] TARGET's PARENT", parent);
+                console.log("[CLICK] TARGET's PARENT DATASET", parent.dataset);
+                sortParameter = parent.dataset.sorting;
 /*
             var sortParameterElement = tableClickEvent.target.querySelector("#SortingParameter");
             if (sortParameterElement) { sortParameter = sortParameterElement.innerText; }
             console.log("[CLICK] INNER SORTING PARAMETER is " + sortParameter);
 */
 
-            if (sortParameter) {
-                SelectContent(sortParameter);
+                if (sortParameter) {
+                    SelectContent(sortParameter);
+                }
             }
         });
-        console.log("[TABLE] Click Listener added for a table");
     });
+    console.log("[TABLE] Click Listener added for a " + tables.length + " tables");
 
 });
 
