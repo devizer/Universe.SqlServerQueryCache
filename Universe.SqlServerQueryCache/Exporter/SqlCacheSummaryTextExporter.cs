e522ccb40c978fef3659f1e691bd36711d7090c9
devizer <devizer@github.io>
2025-02-22 16:35:21 +0200

    Summary: pages in megabytes

--- Universe.SqlServerQueryCache/Exporter/SqlCacheSummaryTextExporter.cs
+++ Universe.SqlServerQueryCache/Exporter/SqlCacheSummaryTextExporter.cs
@@ -11,16 +11,26 @@ namespace Universe.SqlServerQueryCache.Exporter
     {
         public static string Export(IEnumerable<QueryCacheRow> rows, string title)
         {
+            Func<long, string> formatPagesAsString = pages =>
+            {
+                string ret = $"{pages:n0}";
+                var kb = pages * 8192d / 1024;
+                if (pages > 2048) ret += $"  (is {kb / 1024:n0} MB)";
+                else if (pages > 512) ret += $"  (is {kb / 1024:n1} MB)";
+                else if (pages > 0) ret += $"  (is {kb:n2} KB)";
+                return ret;
+            };
+
             StringBuilder ret = new StringBuilder();
             ret.AppendLine($"Summary on {title}");
             ret.AppendLine($"   Queries:             {rows.Count()}");
             ret.AppendLine($"   Execution Count:     {rows.Sum(x => x.ExecutionCount):n0}");
             ret.AppendLine($"   Duration:            {rows.Sum(x => x.TotalElapsedTime / 1000d):n2} milliseconds");
             ret.AppendLine($"   CPU Usage:           {rows.Sum(x => x.TotalWorkerTime / 1000d):n2}");
-            ret.AppendLine($"   Total Pages Read:    {rows.Sum(x => x.TotalLogicalReads):n0}");
-            ret.AppendLine($"   Cached Pages Read:   {rows.Sum(x => Math.Max(0, x.TotalLogicalReads - x.TotalPhysicalReads)):n0}");
-            ret.AppendLine($"   Physical Pages Read: {rows.Sum(x => x.TotalPhysicalReads):n0}");
-            ret.AppendLine($"   Total Pages Writes:  {rows.Sum(x => x.TotalLogicalWrites):n0}");
+            ret.AppendLine($"   Total Pages Read:    {formatPagesAsString(rows.Sum(x => x.TotalLogicalReads))}");
+            ret.AppendLine($"   Cached Pages Read:   {formatPagesAsString(rows.Sum(x => Math.Max(0, x.TotalLogicalReads - x.TotalPhysicalReads)))}");
+            ret.AppendLine($"   Physical Pages Read: {formatPagesAsString(rows.Sum(x => x.TotalPhysicalReads))}");
+            ret.AppendLine($"   Total Pages Writes:  {formatPagesAsString(rows.Sum(x => x.TotalLogicalWrites))}");
             ret.AppendLine($"   The Oldest Lifetime: {rows.Max(x => x.Lifetime)}");
 
             return ret.ToString();
