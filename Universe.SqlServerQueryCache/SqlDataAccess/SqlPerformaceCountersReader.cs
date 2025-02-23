e6fae74bcc4c2557061241bdac68d86e654f3679
devizer <devizer@github.io>
2025-02-23 06:45:27 +0200

    Added SqlPerformanceCountersReader

---
+++ Universe.SqlServerQueryCache/SqlDataAccess/SqlPerformaceCountersReader.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Newtonsoft.Json;
+
+namespace Universe.SqlServerQueryCache.SqlDataAccess
+{
+    public static class SqlPerformanceCountersReaderExtensions
+    {
+        public static SqlBasicPerformanceCounters ReadBasicCounters(this SqlPerformanceCountersReader reader)
+        {
+            var rows = reader.Select("WHERE counter_name in ('Database pages', 'Page reads/sec', 'Page writes/sec') And object_name like '%Buffer Manager%'");
+            var grouped = rows.GroupByCounterName();
+            return new SqlBasicPerformanceCounters()
+            {
+                BufferPages = grouped.FirstOrDefault(x => x.CounterName == "Database pages")?.Value ?? 0L,
+                PageReadsPerSecond = grouped.FirstOrDefault(x => x.CounterName == "Page reads/sec")?.Value ?? 0L,
+                PageWritesPerSecond = grouped.FirstOrDefault(x => x.CounterName == "Page writes/sec")?.Value ?? 0L,
+            };
+        }
+
+
+    }
+
+    public class SqlBasicPerformanceCounters
+    {
+        public long BufferPages { get; set; }
+        public long PageReadsPerSecond { get; set; }
+        public long PageWritesPerSecond { get; set; }
+    }
+
+    public class SqlPerformanceCountersReader
+    {
+        private DbProviderFactory _dbProvider;
+        private string _connectionString;
+
+        public SqlPerformanceCountersReader(DbProviderFactory dbProvider, string connectionString)
+        {
+            _dbProvider = dbProvider ?? throw new ArgumentNullException(nameof(dbProvider));
+            _connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+        }
+
+        public IEnumerable<SqlPerformanceCounterDataRow> Select(string whereClause)
+        {
+            var con = _dbProvider.CreateConnection();
+            con.ConnectionString = _connectionString;
+            var ret = con.Query<SqlPerformanceCounterDataRow>(
+                "Select * From sys.dm_os_performance_counters"
+                + (string.IsNullOrEmpty(whereClause) ? "" : $" {whereClause}"), null);
+
+            return ret;
+        }
+
+        public IEnumerable<SqlPerformanceCounterDataRow> Select()
+        {
+            return Select(string.Empty);
+        }
+    }
+
+    /*
+select * from sys.dm_os_performance_counters where object_name like '%Buffer Node%'
+-- https://learn.microsoft.com/en-us/sql/relational-databases/performance-monitor/use-sql-server-objects?view=sql-server-ver16#SQLServerPOs
+
+SELECT * FROM sys.dm_os_performance_counters WHERE object_name LIKE '%Buffer Manager%';
+-- https://learn.microsoft.com/en-us/sql/relational-databases/performance-monitor/sql-server-buffer-manager-object?view=sql-server-ver16
+
+SELECT * FROM sys.dm_os_performance_counters WHERE counter_name = 'Maximum Workspace Memory (KB)'
+
+Select *
+From sys.dm_os_performance_counters
+Where
+counter_name in ('Database pages', 'Page reads/sec', 'Page writes/sec')
+And object_name like '%Buffer Manager%'
+    */
+
+    public class SqlPerformanceCounterDataRow
+    {
+        public string ObjectName => ObjectName;
+        public string CounterName => Counter_Name;
+        public string InstanceName => Instance_Name;
+        public long Value => Cntr_Value;
+        public long Type => Cntr_Type;
+
+        // Data Access
+        [JsonIgnore]
+        public string Object_Name { get; set; }
+        [JsonIgnore]
+        public string Counter_Name { get; set; }
+        [JsonIgnore]
+        public string Instance_Name { get; set; }
+        [JsonIgnore]
+        public long Cntr_Value { get; set; }
+        [JsonIgnore]
+        public long Cntr_Type { get; set; }
+    }
+
+    public static class SqlPerformanceCounterDataRowExtensions
+    {
+        public static List<SqlPerformanceCounterDataRow> GroupByCounterName(this IEnumerable<SqlPerformanceCounterDataRow> list)
+        {
+            var groups = list.ToLookup(x => $"{x.ObjectName}-->{x.CounterName}");
+            List<SqlPerformanceCounterDataRow> ret = new List<SqlPerformanceCounterDataRow>();
+            foreach (var group in groups)
+            {
+                SqlPerformanceCounterDataRow item = new SqlPerformanceCounterDataRow();
+                foreach (var subItem in group)
+                {
+                    item.Object_Name = subItem.Object_Name;
+                    item.Counter_Name = subItem.Counter_Name;
+                    item.Cntr_Value += subItem.Cntr_Value;
+                    item.Cntr_Type = subItem.Cntr_Type;
+                }
+                ret.Add(item);
+            }
+
+            return ret;
+        }
+    }
+}
--- Universe.SqlServerQueryCache/SqlDataAccess/TheMemoryCacheQuery.cs
+++ Universe.SqlServerQueryCache/SqlDataAccess/TheMemoryCacheQuery.cs
@@ -2,6 +2,23 @@
 
 public static class TheMemoryCacheQuery
 {
+    public const string SqlServerMemoryClerks = @"-- select * from sys.dm_os_memory_clerks /* where type in ('MEMORYCLERK_SQLBUFFERPOOL') */ order by type;
+IF ((@@MICROSOFTVERSION / 16777216) >= 11)
+Exec('Select Type, Name, Sum(pages_kb) pages_kb, Sum(virtual_memory_committed_kb) memory_kb
+From sys.dm_os_memory_clerks
+Where memory_node_id <> 64
+      And (pages_kb > 0 Or virtual_memory_committed_kb > 0)
+Group By Name, Type
+Order By Sum(pages_kb) + Sum(virtual_memory_committed_kb) desc')
+ELSE
+Exec('Select Type, Name, Sum(single_pages_kb + multi_pages_kb) pages_kb, Sum(virtual_memory_committed_kb) memory_kb
+From sys.dm_os_memory_clerks
+Where memory_node_id <> 64
+      And (single_pages_kb > 0 Or multi_pages_kb > 0 Or virtual_memory_committed_kb > 0)
+Group By Name, Type
+Order By Sum(single_pages_kb + multi_pages_kb) + Sum(virtual_memory_committed_kb) desc')";
+
+
     public const string SqlServerMemoryCache = @"IF ((@@MICROSOFTVERSION / 16777216) >= 11)
 Exec('Select Name, Type, sum(pages_kb) kb
 From sys.dm_os_memory_cache_entries
@@ -11,6 +28,10 @@ ELSE
 Exec('Select Name, Type, 8*sum(pages_allocated_count) kb
 From sys.dm_os_memory_cache_entries
 Group By Name, Type
-Having 8*sum(pages_allocated_count) > 0')
-";
+Having 8*sum(pages_allocated_count) > 0')";
 }
+
+// MEMORYCLERK_SQLBUFFERPOOL:
+// This memory clerk keeps track of commonly the largest memory consumer inside SQL Server - data and index pages.
+// Buffer Pool or data cache keeps data and index pages loaded in memory to provide fast access to data.
+// For more information, see Buffer Management.
