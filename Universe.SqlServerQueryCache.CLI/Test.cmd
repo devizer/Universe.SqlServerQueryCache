b759edbde33a41b5cc102e20e44ce7666aa5fbe0
devizer <devizer@github.io>
2025-02-26 16:26:53 +0200

    1) Brush up CLI csproj 2) updated discovery test

--- Universe.SqlServerQueryCache.CLI/Test.cmd
+++ Universe.SqlServerQueryCache.CLI/Test.cmd
@@ -10,3 +10,6 @@ dotnet run -- -o "Query Cache Reports\LOCAL MS SQL SERVER V2.json" -s "(local)"
 echo.
 dotnet run -- -o "Query Cache Reports\LOCAL MS SQL SERVER V2.json" -s "(local)" -av
 
+dotnet run -- -all -av -o "Query Cache Reports\Discovered {InstanceName}" 
+
+
