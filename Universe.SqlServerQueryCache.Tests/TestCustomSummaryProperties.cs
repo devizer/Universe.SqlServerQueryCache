69223e8fd3f0f83998fdaa32acc00b1efc066dbb
devizer <devizer@github.io>
2025-03-05 20:29:15 +0200

    Html Header: Custom Summary Rows, brush up tests

--- Universe.SqlServerQueryCache.Tests/TestCustomSummaryProperties.cs
+++ Universe.SqlServerQueryCache.Tests/TestCustomSummaryProperties.cs
@@ -10,5 +10,14 @@ public class TestCustomSummaryProperties
     {
         var customSummary = CustomSummaryRowReader.GetCustomSummary();
         Console.WriteLine($"CUSTOM SUMMARY:{customSummary.ToJsonString()}");
+        var allEnvVars = Environment.GetEnvironmentVariables().Keys
+            .OfType<object>()
+            .Select(x => Convert.ToString(x))
+            .OrderBy(x => x)
+            .Select(x => new { Var = x, Val = Environment.GetEnvironmentVariable(x) })
+            .ToArray();
+
+        Console.WriteLine($"All Env Vars:{allEnvVars.ToJsonString()}");
+
     }
 }
