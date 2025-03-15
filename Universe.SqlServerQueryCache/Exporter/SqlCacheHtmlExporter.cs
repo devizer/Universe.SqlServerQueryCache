9295f04a1aabb14dee11e33465d4e4ee84b78934
devizer <devizer@github.io>
2025-03-15 06:55:50 +0200

    Extracted SqlServerSummaryBuilder

--- Universe.SqlServerQueryCache/Exporter/SqlCacheHtmlExporter.cs
+++ Universe.SqlServerQueryCache/Exporter/SqlCacheHtmlExporter.cs
@@ -21,7 +21,7 @@ public class SqlCacheHtmlExporter
     public readonly string ConnectionString;
 
     public IEnumerable<QueryCacheRow> Rows { get; protected set; } // Available after Export
-    public IEnumerable<SummaryRow> Summary { get; protected set; } // Available after Export
+    public List<SummaryRow> Summary { get; protected set; } // Available after Export
     private JsStringConstants Strings = new JsStringConstants();
 
     private IEnumerable<TableHeaderDefinition> _tableTopHeaders;
@@ -132,41 +132,9 @@ public class SqlCacheHtmlExporter
     
     private string ExportSummaryAsHtml()
     {
-        List<SummaryRow> summaryRows = SqlSummaryTextExporter.ExportStructured(Rows).ToList();
-        Summary = summaryRows;
-
-        var con = DbProvider.CreateConnection();
-        con.ConnectionString = ConnectionString;
-        var man = con.Manage();
-        var hostPlatform = man.HostPlatform;
-        var mediumVersion = man.MediumServerVersion;
-        // string summaryReportAsText = SqlSummaryTextExporter.ExportAsText(Rows, $"SQL Server {mediumVersion} on {hostPlatform}");
-
-        // SQL Performance Counters
-        SqlPerformanceCountersReader perfReader = new SqlPerformanceCountersReader(DbProvider, ConnectionString);
-        var summaryCounters = perfReader.ReadBasicCounters();
-        if (summaryCounters.BufferPages > 0)
-            summaryRows.Add(new SummaryRow("Database Pages", FormatKind.Pages, summaryCounters.BufferPages));
-        if (summaryCounters.PageReadsPerSecond > 0)
-            summaryRows.Add(new SummaryRow("Page Reads/sec", FormatKind.PagesPerSecond, summaryCounters.PageReadsPerSecond));
-        if (summaryCounters.PageWritesPerSecond > 0)
-            summaryRows.Add(new SummaryRow("Page Writes/sec", FormatKind.PagesPerSecond, summaryCounters.PageWritesPerSecond));
-
-        // Sys Info
-        var summarySysInfo = BuildSysInfoSummary().ToArray();
-        summaryRows.AddRange(summarySysInfo);
-
-        // version
-        var versionRow = new SummaryRow("Version", FormatKind.Unknown, $"{mediumVersion} on {hostPlatform}");
-        // summaryRows.Add(versionRow); // Already on the HTML header
-
-        var customSummaryRows = CustomSummaryRowReader.GetCustomSummary().ToList();
-        foreach (var customSummaryRow in customSummaryRows)
-        {
-            var pos = Math.Max(0, customSummaryRow.Position);
-            pos = Math.Min(summaryRows.Count, pos);
-            if (pos > summaryRows.Count) summaryRows.Add(customSummaryRow); else summaryRows.Insert(pos, customSummaryRow);
-        }
+        SqlServerSummaryBuilder summaryBuilder = new SqlServerSummaryBuilder(DbProvider, ConnectionString, Rows.ToList());
+        Summary = summaryBuilder.BuildTotalWholeSummary();
+        List<SummaryRow> summaryRows = Summary;
 
         StringBuilder ret = new StringBuilder();
         string padding = "\t\t";
@@ -184,78 +152,6 @@ public class SqlCacheHtmlExporter
         return ret.ToString();
     }
 
-    private IEnumerable<SummaryRow> BuildSysInfoSummary()
-    {
-        // Sys Info
-        ICollection<SqlSysInfoReader.Info> sqlSysInfoList = SqlSysInfoReader.Query(DbProvider, ConnectionString);
-        var sqlSysInfo = sqlSysInfoList.ToLookup(x => x.Name).Select(x => new { K = x.Key, V = x.FirstOrDefault()?.Value }).ToDictionary(x => x.K, x => x.V, StringComparer.OrdinalIgnoreCase);
-        Func<string, long?> getLong = name => sqlSysInfo.TryGetValue(name, out var raw) ? Convert.ToInt64(raw) : null;
-        var cpuCount = getLong("cpu_count");
-        if (cpuCount.HasValue) yield return new SummaryRow("CPU Count", FormatKind.Natural, cpuCount.Value);
-        var physical_memory_in_bytes = getLong("physical_memory_in_bytes");
-        var physical_memory_kb = getLong("physical_memory_kb");
-        long? memKb = physical_memory_kb ?? physical_memory_in_bytes / 1024;
-        if (memKb.HasValue) yield return new SummaryRow("Physical Memory (MB)", FormatKind.Natural, memKb.Value / 1024);
-
-        // 2005...2008R2
-        var bpool_Committed = getLong("Bpool_Committed");
-        if (bpool_Committed.HasValue) yield return new SummaryRow("Buffer Pages", FormatKind.Pages, bpool_Committed.Value);
-        var bpool_Commit_Target = getLong("Bpool_Commit_Target");
-        var bpool_Visible = getLong("Bpool_Visible");
-        var visiblePages = GetMin(bpool_Commit_Target, bpool_Visible);
-        if (visiblePages.HasValue) yield return new SummaryRow("Visible Buffer Pages", FormatKind.Pages, visiblePages.Value);
-
-        // 2012+
-        var Committed_Kb = getLong("Committed_Kb");
-        if (Committed_Kb.HasValue) yield return new SummaryRow("Committed Memory (MB)", FormatKind.Natural, Committed_Kb.Value / 1024);
-        var Committed_Target_Kb = getLong("Committed_Target_Kb");
-        var Visible_Target_Kb = getLong("Visible_Target_Kb");
-        var visibleKb = GetMin(Committed_Target_Kb, Visible_Target_Kb);
-        if (visibleKb.HasValue) yield return new SummaryRow("Visible Memory (MB)", FormatKind.Natural, visibleKb.Value / 1024);
-
-        // uptime
-        var sqlserver_start_time_ms_ticks = getLong("sqlserver_start_time_ms_ticks");
-        var Ms_Ticks = getLong("Ms_Ticks");
-        if (sqlserver_start_time_ms_ticks.HasValue && Ms_Ticks.HasValue)
-            yield return new SummaryRow("Uptime", FormatKind.Timespan, TimeSpan.FromMilliseconds(Math.Abs(Ms_Ticks.Value - sqlserver_start_time_ms_ticks.Value)));
-
-        // Total Cpu Usage
-        var process_User_Time_Ms = getLong("Process_User_Time_Ms");
-        if (process_User_Time_Ms.HasValue) yield return new SummaryRow("CPU User Time (seconds)", FormatKind.Numeric2, process_User_Time_Ms.Value / 1000d);
-        var process_Kernel_Time_Ms = getLong("Process_Kernel_Time_Ms");
-        if (process_Kernel_Time_Ms.HasValue) yield return new SummaryRow("CPU Kernel Time (seconds)", FormatKind.Numeric2, process_Kernel_Time_Ms.Value / 1000d);
-
-
-
-
-        var sysInfoKeys = new string[]
-        {
-            "Cpu_Count",
-            "bpool_committed", "physical_memory_in_bytes", // up to 2008 r2
-            "physical_memory_kb", // on azure use process_memory_limit_mb column in sys.dm_os_job_object
-            "committed_kb", // 2012+
-            // sqlserver_start_time_ms_ticks - Ms_Ticks - sql server uptime
-            "sqlserver_start_time_ms_ticks", "Ms_Ticks",
-            // Used and Available memory on 2005...2008R2
-            "Bpool_Committed",
-            "Bpool_Commit_Target",
-            "Bpool_Visible",
-            // Used and Available memory on 2012+
-            "Committed_Kb",
-            "Committed_Target_Kb",
-            "Visible_Target_Kb",
-            // Total CPU Usage on 2012+
-            "Process_Kernel_Time_Ms",
-            "Process_User_Time_Ms",
-        };
-
-    }
-
-    static long? GetMin(params long?[] values)
-    {
-        var notNull = values.Where(x => x.HasValue).ToArray();
-        return notNull.Length == 0 ? null : notNull.Min();
-    }
 
     public string Export(ColumnDefinition sortByColumn, bool isFieldSelected)
     {
@@ -263,12 +159,16 @@ public class SqlCacheHtmlExporter
         var sortedRows = sortByColumn.SortAction(Rows).ToArray();
         StringBuilder htmlTable = new StringBuilder();
         htmlTable.AppendLine("  <table class='Metrics'><thead>");
+        
+        // Table Header: 1st Row (metrics categories)
         htmlTable.AppendLine("  <tr>");
         foreach (var header in headers)
         {
             htmlTable.AppendLine($"    <th colspan='{header.Columns.Count}' class='TableHeaderGroupCell'>{header.Caption}</th>");
         }
         htmlTable.AppendLine("  </tr>");
+        
+        // Table Header: 2nd row (concrete metrics)
         htmlTable.AppendLine("  <tr>");
         var columnDefinitions = headers.SelectMany(h => h.Columns).ToArray();
         foreach (var column in columnDefinitions)
@@ -287,6 +187,7 @@ public class SqlCacheHtmlExporter
         htmlTable.AppendLine("  <tbody>");
         foreach (QueryCacheRow row in sortedRows)
         {
+            // Metrics Row
             htmlTable.AppendLine("  <tr class='MetricsRow'>");
             foreach (ColumnDefinition column in columnDefinitions)
             {
@@ -296,6 +197,7 @@ public class SqlCacheHtmlExporter
             }
             htmlTable.AppendLine("\t</tr>");
             htmlTable.AppendLine("\t<tr class='SqlRow'>");
+            
             // BUILD META LINE as SQL: Database, ObjectType and ObjectName
             StringBuilder sqlMeta = new StringBuilder(); // Use [DB-Stress]; -- For SQL STORED PROCEDURE [Stress By Select]
             if (!string.IsNullOrEmpty(row.DatabaseName)) sqlMeta.Append($"Use [{row.DatabaseName}];");
@@ -304,6 +206,7 @@ public class SqlCacheHtmlExporter
             string htmlObjectSchemaName = string.IsNullOrEmpty(row.ObjectSchemaName) ? "" : $"[{row.ObjectSchemaName}].";
             if (!string.IsNullOrEmpty(row.ObjectName)) sqlMeta.Append(sqlMeta.Length == 0 ? "": " ").Append($"-- For {objectType} {htmlObjectSchemaName}[{row.ObjectName}]".Replace("  [", " ["));
             // Done: sqlMeta
+            
             var rowSqlStatement = sqlMeta.Length == 0 ? row.SqlStatement : $"{sqlMeta}{Environment.NewLine}{row.SqlStatement}";
             var tsqlHtmlString = TSqlToVanillaHtmlConverter.ConvertTSqlToHtml(rowSqlStatement, SqlSyntaxColors.DarkTheme);
             var htmlSqlPlanButton = "";
@@ -319,7 +222,10 @@ public class SqlCacheHtmlExporter
                 htmlSqlPlanButton = $"<div class='SqlPlanDownload' Title='Open Execution Plan' onclick=\"{jsDownloadPlan}; return false;\">{htmlDownloadIcon}</div>";
             }
 
+            // Query Plan Cell (2 columns)
             htmlTable.AppendLine($"\t\t<td colspan='2' class='SqlPadding'>{htmlSqlPlanButton}</td>");
+            
+            // T-SQL Cell (rest of columns)
             htmlTable.AppendLine($"\t\t<td colspan='{columnDefinitions.Length - 2}'><pre>{tsqlHtmlString}</pre></td>");
             htmlTable.AppendLine("\t</tr>");
         }
---
+++ Universe.SqlServerQueryCache/Exporter/SqlServerSummaryBuilder.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Universe.SqlServerJam;
+using Universe.SqlServerQueryCache.SqlDataAccess;
+
+namespace Universe.SqlServerQueryCache.Exporter;
+
+public class SqlServerSummaryBuilder
+{
+    public readonly DbProviderFactory DbProvider;
+    public readonly string ConnectionString;
+    public readonly List<QueryCacheRow> Rows;
+
+    public SqlServerSummaryBuilder(DbProviderFactory dbProvider, string connectionString, List<QueryCacheRow> rows)
+    {
+        DbProvider = dbProvider;
+        ConnectionString = connectionString;
+        Rows = rows;
+    }
+
+    public List<SummaryRow> BuildTotalWholeSummary()
+    {
+        List<SummaryRow> summaryRows = SqlSummaryTextExporter.ExportStructured(Rows).ToList();
+
+        var con = DbProvider.CreateConnection();
+        con.ConnectionString = ConnectionString;
+        var man = con.Manage();
+        var hostPlatform = man.HostPlatform;
+        var mediumVersion = man.MediumServerVersion;
+        // string summaryReportAsText = SqlSummaryTextExporter.ExportAsText(Rows, $"SQL Server {mediumVersion} on {hostPlatform}");
+
+        // SQL Performance Counters
+        SqlPerformanceCountersReader perfReader = new SqlPerformanceCountersReader(DbProvider, ConnectionString);
+        var summaryCounters = perfReader.ReadBasicCounters();
+        if (summaryCounters.BufferPages > 0)
+            summaryRows.Add(new SummaryRow("Database Pages", FormatKind.Pages, summaryCounters.BufferPages));
+        if (summaryCounters.PageReadsPerSecond > 0)
+            summaryRows.Add(new SummaryRow("Page Reads/sec", FormatKind.PagesPerSecond, summaryCounters.PageReadsPerSecond));
+        if (summaryCounters.PageWritesPerSecond > 0)
+            summaryRows.Add(new SummaryRow("Page Writes/sec", FormatKind.PagesPerSecond, summaryCounters.PageWritesPerSecond));
+
+        // Sys Info
+        var summarySysInfo = BuildSysInfoSummary().ToArray();
+        summaryRows.AddRange(summarySysInfo);
+
+        // version
+        var versionRow = new SummaryRow("Version", FormatKind.Unknown, $"{mediumVersion} on {hostPlatform}");
+        // summaryRows.Add(versionRow); // Already on the HTML header
+
+        var customSummaryRows = CustomSummaryRowReader.GetCustomSummary().ToList();
+        foreach (var customSummaryRow in customSummaryRows)
+        {
+            var pos = Math.Max(0, customSummaryRow.Position);
+            pos = Math.Min(summaryRows.Count, pos);
+            if (pos > summaryRows.Count) summaryRows.Add(customSummaryRow);
+            else summaryRows.Insert(pos, customSummaryRow);
+        }
+
+        return summaryRows;
+    }
+
+    public IEnumerable<SummaryRow> BuildSysInfoSummary()
+    {
+        // Sys Info
+        ICollection<SqlSysInfoReader.Info> sqlSysInfoList = SqlSysInfoReader.Query(DbProvider, ConnectionString);
+        var sqlSysInfo = sqlSysInfoList.ToLookup(x => x.Name).Select(x => new { K = x.Key, V = x.FirstOrDefault()?.Value }).ToDictionary(x => x.K, x => x.V, StringComparer.OrdinalIgnoreCase);
+        Func<string, long?> getLong = name => sqlSysInfo.TryGetValue(name, out var raw) ? Convert.ToInt64(raw) : null;
+        var cpuCount = getLong("cpu_count");
+        if (cpuCount.HasValue) yield return new SummaryRow("CPU Count", FormatKind.Natural, cpuCount.Value);
+        var physical_memory_in_bytes = getLong("physical_memory_in_bytes");
+        var physical_memory_kb = getLong("physical_memory_kb");
+        long? memKb = physical_memory_kb ?? physical_memory_in_bytes / 1024;
+        if (memKb.HasValue) yield return new SummaryRow("Physical Memory (MB)", FormatKind.Natural, memKb.Value / 1024);
+
+        // 2005...2008R2
+        var bpool_Committed = getLong("Bpool_Committed");
+        if (bpool_Committed.HasValue) yield return new SummaryRow("Buffer Pages", FormatKind.Pages, bpool_Committed.Value);
+        var bpool_Commit_Target = getLong("Bpool_Commit_Target");
+        var bpool_Visible = getLong("Bpool_Visible");
+        var visiblePages = GetMin(bpool_Commit_Target, bpool_Visible);
+        if (visiblePages.HasValue) yield return new SummaryRow("Visible Buffer Pages", FormatKind.Pages, visiblePages.Value);
+
+        // 2012+
+        var Committed_Kb = getLong("Committed_Kb");
+        if (Committed_Kb.HasValue) yield return new SummaryRow("Committed Memory (MB)", FormatKind.Natural, Committed_Kb.Value / 1024);
+        var Committed_Target_Kb = getLong("Committed_Target_Kb");
+        var Visible_Target_Kb = getLong("Visible_Target_Kb");
+        var visibleKb = GetMin(Committed_Target_Kb, Visible_Target_Kb);
+        if (visibleKb.HasValue) yield return new SummaryRow("Visible Memory (MB)", FormatKind.Natural, visibleKb.Value / 1024);
+
+        // uptime
+        var sqlserver_start_time_ms_ticks = getLong("sqlserver_start_time_ms_ticks");
+        var Ms_Ticks = getLong("Ms_Ticks");
+        if (sqlserver_start_time_ms_ticks.HasValue && Ms_Ticks.HasValue)
+            yield return new SummaryRow("Uptime", FormatKind.Timespan, TimeSpan.FromMilliseconds(Math.Abs(Ms_Ticks.Value - sqlserver_start_time_ms_ticks.Value)));
+
+        // Total Cpu Usage
+        var process_User_Time_Ms = getLong("Process_User_Time_Ms");
+        if (process_User_Time_Ms.HasValue) yield return new SummaryRow("CPU User Time (seconds)", FormatKind.Numeric2, process_User_Time_Ms.Value / 1000d);
+        var process_Kernel_Time_Ms = getLong("Process_Kernel_Time_Ms");
+        if (process_Kernel_Time_Ms.HasValue) yield return new SummaryRow("CPU Kernel Time (seconds)", FormatKind.Numeric2, process_Kernel_Time_Ms.Value / 1000d);
+
+
+
+
+        var sysInfoKeys = new string[]
+        {
+            "Cpu_Count",
+            "bpool_committed", "physical_memory_in_bytes", // up to 2008 r2
+            "physical_memory_kb", // on azure use process_memory_limit_mb column in sys.dm_os_job_object
+            "committed_kb", // 2012+
+            // sqlserver_start_time_ms_ticks - Ms_Ticks - sql server uptime
+            "sqlserver_start_time_ms_ticks", "Ms_Ticks",
+            // Used and Available memory on 2005...2008R2
+            "Bpool_Committed",
+            "Bpool_Commit_Target",
+            "Bpool_Visible",
+            // Used and Available memory on 2012+
+            "Committed_Kb",
+            "Committed_Target_Kb",
+            "Visible_Target_Kb",
+            // Total CPU Usage on 2012+
+            "Process_Kernel_Time_Ms",
+            "Process_User_Time_Ms",
+        };
+    }
+
+    static long? GetMin(params long?[] values)
+    {
+        var notNull = values.Where(x => x.HasValue).ToArray();
+        return notNull.Length == 0 ? null : notNull.Min();
+    }
+
+
+
+}
